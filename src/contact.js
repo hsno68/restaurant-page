345c030cc58f10703b49827ca19f3f031a2c9dd7
haiseno <haise.zhao@gmail.com>
2025-04-22 00:53:07 -0700

    Refactor contact page to element builder

--- src/contact.js
+++ src/contact.js
@@ -1,35 +1,20 @@
+import ElementBuilder, { contentContainer } from "./builderClass.js";
+
 export default function contactPageLoad() {
-  const contentContainer = document.querySelector("div#content-container");
   contentContainer.replaceChildren();
 
-  const contactHeading = document.createElement("h1");
-  contactHeading.textContent = "Contact Us";
-  contentContainer.appendChild(contactHeading);
+  const headingSection = new ElementBuilder("div")
+    .child(new ElementBuilder("h1").text("Contact").build())
+    .build();
 
-  const locationOneHeading = document.createElement("h2");
-  locationOneHeading.textContent = "First Location";
-  const locationOnePara = document.createElement("p");
-  locationOnePara.innerText = `Location One
-  123 Location Drive, State, CA, 00000
-  555-555-5555
-  contactEmail@email.com`;
-  contentContainer.append(locationOneHeading, locationOnePara);
+  const detailsSection = new ElementBuilder("div")
+    .html(
+     `<p>Location Two<br/>
+      123 Location Drive, State, CA, 00000<br/>
+      555-555-5555<br/>
+      contactEmail@email.com<br/>
+      </p>`)
+    .build();
 
-  const locationTwoHeading = document.createElement("h2");
-  locationTwoHeading.textContent = "Second Location";
-  const locationTwoPara = document.createElement("p");
-  locationTwoPara.innerText = `Location Two
-  123 Location Drive, State, CA, 00000
-  555-555-5555
-  contactEmail@email.com`;
-  contentContainer.append(locationTwoHeading, locationTwoPara);
-
-  const locationThreeHeading = document.createElement("h2");
-  locationThreeHeading.textContent = "Third Location";
-  const locationThreePara = document.createElement("p");
-  locationThreePara.innerText = `Location Three
-  123 Location Drive, State, CA, 00000
-  555-555-5555
-  contactEmail@email.com`;
-  contentContainer.append(locationThreeHeading, locationThreePara);
+  contentContainer.append(headingSection, detailsSection);
 }
