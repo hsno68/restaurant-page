b0b6ba52afec7e6fccd4bc5f04c0f462a7292cf4
haiseno <haise.zhao@gmail.com>
2025-04-21 21:11:51 -0700

    Refactor DOM logic
    Refactor code from hard coding each section with
    individual DOM manipulation methods to reusuable
    class that builds sections.

--- src/homepage.js
+++ src/homepage.js
@@ -2,44 +2,58 @@ export default function homePageLoad() {
   const contentContainer = document.querySelector("div#content-container");
   contentContainer.replaceChildren();
 
-  const restaurantNameHeading = document.createElement("h1");
-  restaurantNameHeading.textContent = "Restaurant Name";
-  contentContainer.appendChild(restaurantNameHeading);
+  const pageContent = [];
 
-  const aboutHeading = document.createElement("h2");
-  aboutHeading.textContent = "About";
-  contentContainer.appendChild(aboutHeading);
-  
-  const aboutPara = document.createElement("p");
-  aboutPara.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida, mi in varius gravida, ante ex tempor ex, sit amet tincidunt turpis orci ut metus. Ut tortor ligula, cursus non aliquam in, elementum nec ante. Donec maximus luctus odio at sagittis. Fusce iaculis cursus turpis, sed viverra lectus lobortis a.";
-  contentContainer.appendChild(aboutPara);
+  class ElementBuilder {
+    constructor(type) {
+      this.element = document.createElement(type);
+    }
 
-  const hoursHeading = document.createElement("h2");
-  hoursHeading.textContent = "Hours";
-  contentContainer.appendChild(hoursHeading);
+    text(content) {
+      this.element.textContent = content;
+      return this;
+    }
 
-  const hoursList = document.createElement("ul");
-  const daysOpen = {
-    Monday: "9am - 10pm",
-    Tuesday: "9am - 10pm",
-    Wednesday: "9am - 10pm",
-    Thursday: "9am - 10pm",
-    Friday: "9am - 11pm",
-    Saturday: "9am - 11pm",
-    Sunday: "9am - 11pm",
-  };
-  for (const day in daysOpen) {
-    const dayListItem = document.createElement("li");
-    dayListItem.textContent = `${day} : ${daysOpen[day]}`;
-    hoursList.appendChild(dayListItem);
+    html(content) {
+      this.element.innerHTML = content;
+      return this;
+    }
+
+    child(child) {
+      this.element.appendChild(child);
+      return this;
+    }
+
+    build() {
+      return this.element;
+    }
   }
-  contentContainer.appendChild(hoursList);
 
-  const locationHeading = document.createElement("h2");
-  locationHeading.textContent = "Location";
-  contentContainer.appendChild(locationHeading);
+  const restaurantName = new ElementBuilder("div")
+    .child(new ElementBuilder("h1").text("Restaurant Name").build())
+    .build();
 
-  const locationPara = document.createElement("p");
-  locationPara.textContent = "123 Location Drive, Location, CA, 00000";
-  contentContainer.appendChild(locationPara);
+  const aboutSection = new ElementBuilder("div")
+    .child(new ElementBuilder("h2").text("About").build())
+    .child(new ElementBuilder("p").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida, mi in varius gravida, ante ex tempor ex, sit amet tincidunt turpis orci ut metus. Ut tortor ligula, cursus non aliquam in, elementum nec ante. Donec maximus luctus odio at sagittis. Fusce iaculis cursus turpis, sed viverra lectus lobortis a.").build())
+    .build();
+
+  const timeSection = new ElementBuilder("div")
+    .child(new ElementBuilder("h2").text("Hours").build())
+    .child(new ElementBuilder("ul").html(
+     `<li>Monday: 9am - 10pm</li>
+      <li>Tuesday: 9am - 10pm</li>
+      <li>Wednesday: 9am - 10pm</li>
+      <li>Thursday: 9am - 10pm</li>
+      <li>Friday: 9am - 11pm</li>
+      <li>Saturday: 9am - 11pm</li>
+      <li>Sunday: 9am - 11pm</li>`).build())
+    .build();
+
+  const locationSection = new ElementBuilder("div")
+    .child(new ElementBuilder("h2").text("Location").build())
+    .child(new ElementBuilder("p").text("123 Location Drive, Location, CA, 00000").build())
+    .build();
+
+  contentContainer.append(restaurantName, aboutSection, timeSection, locationSection);
 }
